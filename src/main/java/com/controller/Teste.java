02ebbf5947518d5817207c45839eb379ad53c09b
FernandoGCallachi <fernando.rocha@mercadolivre.com>
2022-01-07 22:27:22 -0300

    Refact class

--- src/main/java/com/controller/Teste.java
+++ src/main/java/com/controller/Teste.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class teste {
+public class Teste {
 
     @RequestMapping("/teste")
     public String teste(){
